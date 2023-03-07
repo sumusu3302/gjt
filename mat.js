e2c8af87188fcf2dc12ad8931570cc8d52bce441
sumusu3302 <124745973+sumusu3302@users.noreply.github.com>
2023-03-07 09:26:18 +0900

    Create mat.js

---
+++ mat.js
@@ -0,0 +1,5 @@
+var s = window.screen;
+  var width = q.width = window.innerWidth;
+  var height = q.height = window.innerHeight;
+  var letters = Array(256).join(1).split('');
+  // var matrix = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789";
